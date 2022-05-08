ae0efce8aa14a6306aa3f85451ea01eeba5041c2
Ekaterina <kitekate2000cat@gmail.com>
2022-05-08 15:04:45 +0300

    refactoring

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -11,21 +11,11 @@ public class CameraController:MonoBehaviour
     [SerializeField] private bool keyboardMove = GameSettings.KEYBOARD_NAVIGATION_ENABLED;
 
     private Camera _camera;
-    private float _screenWidth;
-    private float _screenHeight;
-    private float _borderMultiplier = 0.05f;
-    private float _borderW;
-    private float _borderH;
-    private float _border;
+    private readonly float _borderMargin = 0.05f;
 
     private void Start()
     {
         _camera = gameObject.GetComponent<Camera>();
-        _screenHeight = _camera.pixelHeight;
-        _screenWidth = _camera.pixelWidth;
-        _borderH = _borderMultiplier * _screenHeight;
-        _borderW = _borderMultiplier * _screenWidth;
-        _border = 25;
     }
 
     private void Update()
@@ -50,18 +40,14 @@ public class CameraController:MonoBehaviour
         
         float x = mousePosition.x;
         float y = mousePosition.y;
-        
         if (x < 0 || y < 0 || x > 1 || y > 1) return;
-        
-        // float xInput = _borderMultiplier / (mousePosition.x > 0.5f ? 1 - mousePosition.x : -mousePosition.x) * 0.2f;
-        // float yInput = _borderMultiplier / (mousePosition.y > 0.5f ?1 - mousePosition.y : -mousePosition.y)* 0.2f;
         
         float xInput = mousePosition.x > 0.5f ? 1 :-1;
         float yInput = mousePosition.y > 0.5f ? 1 :-1;
         
-        if (x < _borderMultiplier || 1 - x < _borderMultiplier)
+        if (x < _borderMargin || 1 - x < _borderMargin)
             Move(xInput, 0);
-        if(y < _borderMultiplier || 1 - y < _borderMultiplier)
+        if(y < _borderMargin || 1 - y < _borderMargin)
             Move(0, yInput);
     }
 
@@ -75,18 +61,10 @@ public class CameraController:MonoBehaviour
     {
         float scrollInput = Input.mouseScrollDelta.y;
         if(scrollInput == 0) return;
-        //float distance = Vector3.Distance(transform.position, _camera.transform.position);
 
         float size =_camera.orthographicSize - zoomSpeed * scrollInput;
-
         if (size < minCameraSize || size > maxCameraSize) return;
 
         _camera.orthographicSize = size;
-
-    }
-
-    public void FocusOnPosition(Vector3 position)
-    {
-        transform.position = position;
     }
 }
